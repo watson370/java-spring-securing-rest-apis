57c199169ac2f09a66bb693c2e1bc31d0f49069f
watson370 <ryan.watson@me.com>
2021-05-03 11:06:59 -0400

    module 3

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ public class ResolutionController {
 	}
 
 	@GetMapping("/resolutions")
+	@CrossOrigin(maxAge = 0, allowCredentials = "true")//remove max age after finished with module
 	@PreAuthorize("hasAuthority('resolution:read')")
 	@PostFilter("@post.filter(#root)")
 	public Iterable<Resolution> read() {
--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -7,24 +7,43 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionsApplication.class, args);
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(authz -> authz.anyRequest().authenticated()).httpBasic(basic -> {
-		});
-		http.csrf().disable();// todo
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authz -> authz.anyRequest()
+                .authenticated())
+                .httpBasic(basic -> {
+                })
+                .cors(cors -> {//somehow this configures spring security to allow CORS handshake
+                });
+        http.csrf().disable();// todo
+    }
 
-	@Bean
-	public UserDetailsService userDetailsService(UserRepository users) {
-		return new UserRepositoryUserDetailsService(users);
-	}
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository users) {
+        return new UserRepositoryUserDetailsService(users);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4000")
+                        .allowedMethods("HEAD")
+                        .allowedHeaders("Authorization");
+            }
+        };
+    }
 }
